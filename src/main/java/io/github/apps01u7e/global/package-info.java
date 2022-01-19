c1bb1b795d2c22cddd08fffe990879c8b9a2fb44
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-01-19 11:11:33 +0900

    [ADD] 기본 프로젝트 프레임 구성

---
+++ src/main/java/io/github/apps01u7e/global/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * protoCS-java 라이브러리 내부에서 전반적으로 사용되는 객체들을 모아놓은 패키지 입니다.
+ *
+ * @author JeeInho
+ * @since 0.0.1alpha-RELEASE
+ * @version 0.0.1alpha-RELEASE
+ */
+
+package io.github.apps01u7e.global;
---
+++ src/main/java/io/github/apps01u7e/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * protoCS-java 라이브러리의 기본 패키지 입니다,
+ *
+ * @author JeeInho
+ * @since 0.0.1alpha-RELEASE
+ * @version 0.0.1alpha-RELEASE
+ */
+
+package io.github.apps01u7e;
