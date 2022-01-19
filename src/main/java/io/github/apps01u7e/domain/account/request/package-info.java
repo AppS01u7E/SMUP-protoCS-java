a1da26c26af5898de0afe0a9e77af09e96b74907
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-01-19 14:55:41 +0900

    [ADD] 계정 도메인 패키지와 해당 패키지의 하위 패키지에 대한 package-info 작성

---
+++ src/main/java/io/github/apps01u7e/domain/account/request/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * soom account api 에서의 모든 요청에 필요한 통신객체들을 제공하는 패키지 입니다.
+ *
+ * @author JeeInho
+ * @since 0.0.1alpha-RELEASE
+ * @version 0.0.1alpha-RELEASE
+ */
+
+package io.github.apps01u7e.domain.account.request;
